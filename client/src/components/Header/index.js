ee196836f94d53fd98de322065739be8904d14c7
Parker Morgan <jpmaster13@msn.com>
2021-02-19 18:45:19 -0700

    Header base file

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../Navbar"
+
+
+function Header(props) {
+  return (
+    <div>
+        <Navbar />
+        <p>Header Stuff</p>
+    </div>
+  );
+}
+
+export default Header;

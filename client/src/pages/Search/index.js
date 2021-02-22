a214ef9e13efcb4a368c4a5f851baa0a850df8e3
Parker Morgan <jpmaster13@msn.com>
2021-02-22 11:39:09 -0700

    Pages for react routes

---
+++ client/src/pages/Search/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BookList from "../../components/BookList";
+import SearchBar from "../../components/Searchbar";
+
+function Search() {
+  return (
+    <>
+      <BookList />
+      <SearchBar />
+    </>
+  );
+}
+
+export default Search;

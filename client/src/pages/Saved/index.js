d46ae9ecc847196dd4dd99f9d38df06028e69fec
Parker Morgan <jpmaster13@msn.com>
2021-02-23 15:00:49 -0700

    Pages added more state consts

--- client/src/pages/Saved/index.js
+++ client/src/pages/Saved/index.js
@@ -1,10 +1,28 @@
-import React from "react";
-import BookList from "../../components/BookList";
+import React, { useState, useEffect } from "react";
+import BookSaved from "../../components/BookSaved";
+import API from "../../utils/API"
+
+
 
 function Saved() {
+  const [books, setBooks] = useState([])
+
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    loadBooks()
+  }, [])
+
+  // Loads all books and sets them to books
+  function loadBooks() {
+    API.getBooks()
+      .then(res => 
+        setBooks(res.data)
+      )
+      .catch(err => console.log(err));
+  };
   return (
     <>
-      <BookList />
+      <BookSaved books={books} />
     </>
   );
 }
--- client/src/pages/Search/index.js
+++ client/src/pages/Search/index.js
@@ -1,12 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import BookList from "../../components/BookList";
 import SearchBar from "../../components/Searchbar";
+import useDebounce from "../../utils/debounceHook";
+import API from "../../utils/API";
 
 function Search() {
+  const [search, setSearch] = useState("Googlebooks");
+  // const [title, setTitle] = useState("");
+  // const [url, setUrl] = useState("");
+  const [error, setError] = useState("");
+  const [book, setBook] = useState([]);
+  const [formObject, setFormObject] = useState({
+    title: "",
+    author: "",
+    link: "",
+    imageLink: ""
+  })
+
+  const debouncedSearchTerm = useDebounce(search, 500);
+
+  useEffect(() => {
+    if (!search) {
+      return;
+    }
+
+    if (debouncedSearchTerm) {
+      API.search(search)
+        .then((res) => {
+          if (res.data.length === 0) {
+            throw new Error("No results found.");
+          }
+          if (res.data.status === "error") {
+            throw new Error(res.data.message);
+          }
+          setBook(res.data);
+        })
+        .catch((err) => setError(err));
+    }
+  }, [debouncedSearchTerm]);
+
+  const handleInputChange = (event) => {
+    setSearch(event.target.value);
+  };
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(event.target)
+    API.saveBook({
+      title: formObject.title,
+      author: formObject.author,
+      link: formObject.link,
+      img: formObject.img,
+    })
+      .then(() =>
+        setFormObject({
+          title: "",
+          author: "",
+          link: "",
+          imageLink: ""
+        })
+      )
+      .then(console.log("Book Saved"))
+      .catch((err) => console.log(err));
+  }
+
   return (
     <>
-      <BookList />
-      <SearchBar />
+      <SearchBar 
+      handleInputChange={handleInputChange} results={search} />
+      <div>{error}</div>
+      <BookList 
+      books={book} 
+      handleFormSubmit={handleFormSubmit} 
+      setFormObject={setFormObject}
+      />
     </>
   );
 }

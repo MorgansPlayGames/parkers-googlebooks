620bbf3ce1b2a7a3c25fb1ff20e7238c9a58eed2
Parker Morgan <jpmaster13@msn.com>
2021-02-23 15:01:35 -0700

    updated API routes and added a cool debounce hook

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,5 +5,16 @@ const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
 export default {
   search: function(query) {
     return axios.get(BASEURL + query);
+  },
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
   }
 };

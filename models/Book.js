b0af81486faa11888aaf122340e8602b497f667f
Parker Morgan <jpmaster13@msn.com>
2021-02-23 14:52:59 -0700

    Fixed book schema to match data passed in.

--- models/Book.js
+++ models/Book.js
@@ -3,9 +3,8 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  authors: { type: String, required: true },
-  description: String,
-  image: { type: Date, default: Date.now },
+  author: { type: String },
+  imageLink: { type: String },
   link: String
 });
 

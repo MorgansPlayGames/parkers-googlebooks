5d8a6f7873c9a927fcf35c4902b41cd923d26e27
Parker Morgan <jpmaster13@msn.com>
2021-02-23 14:56:57 -0700

    Updated props to include onChange and onSubmit

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,16 +1,35 @@
-import React from "react";
-import Book from "../Book"
+import React, { useState } from "react";
+import Book from "../Book";
+
 
 
 function BookList(props) {
-  return (
-    <div>
-        <p>Book1</p>
-        <Book />
-        <p>Book2</p>
-        <Book />
-    </div>
-  );
-}
+  const [books, setBooks] = useState(["","",""]);
+  let HFS = props.handleFormSubmit
+  let setFormObject = props.setFormObject;
+  if(props.handleFormSubmit){
+    HFS = props.handleFormSubmit
+  } else {
+    HFS="#"
+    setFormObject="#"
+  }
 
+  if(props.books.items!=books){
+    setBooks(props.books.items)
+  } 
+  if(books){
+    return(
+      <div>
+        
+          <Book books={books[0]} handleFormSubmit={HFS} setFormObject={setFormObject} />
+          <Book books={books[1]} handleFormSubmit={HFS} setFormObject={setFormObject} />
+          <Book books={books[2]} handleFormSubmit={HFS} setFormObject={setFormObject} />
+        
+      </div>
+    )
+  } else {
+    return <div>search books</div>
+  }
+  
+}
 export default BookList;

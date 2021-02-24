7741600d58eb4cdedab599c3588f2c2c9a8f2e61
Parker Morgan <jpmaster13@msn.com>
2021-02-23 18:45:38 -0700

    renamed all passed props

--- client/src/components/BookList/index.js
+++ client/src/components/BookList/index.js
@@ -1,35 +1,42 @@
 import React, { useState } from "react";
 import Book from "../Book";
 
-
-
 function BookList(props) {
-  const [books, setBooks] = useState(["","",""]);
-  let HFS = props.handleFormSubmit
+  const [books, setBooks] = useState(["", "", ""]);
+  let HFS = props.handleFormSubmit;
   let setFormObject = props.setFormObject;
-  if(props.handleFormSubmit){
-    HFS = props.handleFormSubmit
+  if (props.handleFormSubmit) {
+    HFS = props.handleFormSubmit;
   } else {
-    HFS="#"
-    setFormObject="#"
+    HFS = "#";
+    setFormObject = "#";
   }
 
-  if(props.books.items!=books){
-    setBooks(props.books.items)
-  } 
-  if(books){
-    return(
-      <div>
-        
-          <Book books={books[0]} handleFormSubmit={HFS} setFormObject={setFormObject} />
-          <Book books={books[1]} handleFormSubmit={HFS} setFormObject={setFormObject} />
-          <Book books={books[2]} handleFormSubmit={HFS} setFormObject={setFormObject} />
-        
-      </div>
-    )
-  } else {
-    return <div>search books</div>
+  if (props.books.items != books) {
+    setBooks(props.books.items);
   }
-  
+  if (books) {
+    return (
+      <div>
+        <Book
+          books={books[0]}
+          handleFormSubmit={HFS}
+          setFormObject={setFormObject}
+        />
+        <Book
+          books={books[1]}
+          handleFormSubmit={HFS}
+          setFormObject={setFormObject}
+        />
+        <Book
+          books={books[2]}
+          handleFormSubmit={HFS}
+          setFormObject={setFormObject}
+        />
+      </div>
+    );
+  } else {
+    return <div>search books</div>;
+  }
 }
 export default BookList;
